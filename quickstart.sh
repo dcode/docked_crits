f803ddf397f2fb3fb1b6442a14cc5452348eba3b
KnifeHands <adamkniffen@gmail.com>
2016-01-29 08:26:47 -0600

    moar adjustments for port mappings, prep for separate nginx and uwsgi app

--- quickstart.sh
+++ quickstart.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # build crits-web
 docker build -t crits-web .
-# start mongo
+echo "Starting percona-server-mongodb"
 docker run -d --name mongodb percona/percona-server-mongodb:latest
-# run crits-web and execute this script
+echo "running crits with link to mongodb"
 docker run --rm --link mongodb:mongodb crits-web python /data/crits/manage.py create_default_collections
-docker run --rm --link mongodb:mongodb crits-web python /data/crits/manage.py users -a -A -e admin@foo.org -f admin -l admin -o foo -u admin
-docker-compose up 
+docker exec crits-web python /data/crits/manage.py users -a -A -e admin@foo.org -f admin -l admin -o foo -u admin
